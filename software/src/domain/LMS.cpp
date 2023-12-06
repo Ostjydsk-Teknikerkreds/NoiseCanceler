ab31b2bb29216a1f5f52bb68b766afcbe67a0f5d
Markus Rytter <markus.r@live.dk>
2023-12-06 11:42:49 +0100

    LMS evaluate

--- software/src/domain/LMS.cpp
+++ software/src/domain/LMS.cpp
@@ -1,6 +1,22 @@
 #include "LMS.h"
 
-LMS::LMS(uint8_t inputs) : weights(inputs)
-{
+#include <cassert>
 
+LMS::LMS(uint8_t inputs) : _weights(inputs)
+{
+    for(uint8_t i = 0; i < inputs; i++)
+    {
+        _weights.at(i) = 1.f;
+    }
+}
+
+float LMS::evaluate(std::vector<float> inputs)
+{
+    assert(_weights.size() == inputs.size());
+    float ret = 0;
+    for(uint8_t i = 0; i < inputs.size(); i++)
+    {
+        ret += inputs.at(i) * _weights.at(i);
+    }
+    return ret;
 }
--- software/src/domain/LMS.h
+++ software/src/domain/LMS.h
@@ -5,7 +5,8 @@ class LMS
 {
   public:
     LMS(uint8_t inputs);
+    float evaluate(std::vector<float> inputs);
   protected:
   private:
-    std::vector<float> weights;
+    std::vector<float> _weights;
 };
