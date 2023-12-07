d52c97d335735f093024a64a2d8b2af8e99a0292
Markus Rytter <markus.r@live.dk>
2023-12-07 22:02:31 +0100

    Project compiling

--- software/scripts/cmake_build_debug.sh
+++ software/scripts/cmake_build_debug.sh
@@ -1,24 +1,10 @@
-#! /bin/bash
-
-dryrun=""
-while getopts "d" options; do
-    case "$options" in
-        d)  dryrun="1";;
-        ?)  printf "Usage: %s: [-d]\n" $0
-            exit 2;;
-    esac
-done
-
-build_output_dir=.cmake_build_debug
-threads=8
+build_output_dir=build
 
 cmake_options=()
-cmake_options+=(-DCMAKE_BUILD_TYPE:STRING=Debug)
-cmake_options+=(-DCMAKE_VERBOSE_MAKEFILE:BOOL=FALSE)
+#cmake_options+=(-DCMAKE_BUILD_TYPE:STRING=Debug)
+#cmake_options+=(-DCMAKE_VERBOSE_MAKEFILE:BOOL=FALSE)
 
 mkdir -p ${build_output_dir}
 cd ${build_output_dir}
-cmake ${cmake_options[@]} .. || exit $?
-if [ -z "$dryrun" ]; then
-    cmake --build . -- -j ${threads} || exit $?
-fi
+cmake -G Ninja ${cmake_options[@]}..
+ninja
